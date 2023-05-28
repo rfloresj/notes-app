62d439deb9993239845d88a8870e36420d36a6af
Roberto Carlos Flores Juarez <rfloresj@alsea.com.mx>
2023-05-27 19:30:57 -0600

    Adding engine and modules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const  path = require('path');
+const exphbs = require('express-handlebars');
 
 //  Initializations
 const app = express();
 
 //  Settings
-app.set('port', process.env.PORT || 3000); // process.env.PORT nos indica que si existe un puerto libre en nuestra computadora, lo tome
-// de lo contrario toma el 3000.
+app.set('port', process.env.PORT || 3000); // process.env.PORT nos indica que si existe un puerto libre en nuestra computadora, lo tome de lo contrario toma el 3000.
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
 
 // Middlewares
+app.use()
+
 
 // Global Variables
 
