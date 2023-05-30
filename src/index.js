173024bcf8976c4482108be55961f4415d6a8747
Roberto Flores <txlinemusic@gmail.com>
2023-05-30 12:30:21 -0600

    SignIn Completed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 
 //  Initializations
 const app = express();
 require('./database');
+require('./config/passport');
 
 //  Settings
 app.set('port', process.env.PORT || 3000); // process.env.PORT nos indica que si existe un puerto libre en nuestra computadora, lo tome de lo contrario toma el 3000.
@@ -29,13 +31,16 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
+
 
 // Global Variables
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
-    
+    res.locals.error = req.flash('error');
     next();
 });
 
