7bac5a797119655a0c0182cd6349b95b0a3845b2
Roberto Flores <txlinemusic@gmail.com>
2023-05-29 18:36:43 -0600

    Adding notes + CRUD

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/Note');
+
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note');
 })
 
-router.get('/notes', (req, res) => {
-    res.send('Notes from database');
+router.post('/notes/new-note', async (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'Please Write a Title'});
+    }
+    if(!description){
+        errors.push({text: 'Please Write a Description'});
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note',{
+        errors,
+        title,
+        description
+        });
+    } else {
+        const newNote = new Note({title, description});
+        await newNote.save();
+        res.redirect('/notes')
+    }
+});
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find().lean().sort({date: 'desc'});
+    res.render('notes/all-notes', { notes });
+});
+
+router.get('/notes/edit/:id', async (req, res) => {
+    const note = await Note.findById(req.params.id).lean();
+    res.render('notes/edit-note', {note});
 });
 
 module.exports = router;
