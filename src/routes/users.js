fe15154e6029fd6e5ad2abaf335dcb3f36329150
Roberto Flores <txlinemusic@gmail.com>
2023-05-29 22:58:36 -0600

    Create users (Signup)

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
 
 router.get('/users/signin', (req, res) => {
     res.render('users/signin');
@@ -8,6 +9,34 @@ router.get('/users/signin', (req, res) => {
 
 router.get('/users/signup', (req, res) => {
     res.render('users/signup');
+});
+
+router.post('/users/signup', async (req, res) => {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+    if(name.length <= 0){
+        errors.push({text: 'Please insert your name'});
+    }
+    if(password != confirm_password){
+        errors.push({text: 'Password do not match'});
+    }
+    if(password.length < 4 ){
+        errors.push({text: 'Password must be at least 4 characters'});
+    }
+    if(errors.length > 0){
+        res.render('users/signup', {errors, name, email, password, confirm_password});
+    } else {
+        const emailUser = await User.findOne({email: email});
+        if(emailUser){
+            req.flash('error_msg', 'The email is already in use');
+            res.redirect('/users/signup');
+        };
+        const newUser = new User({name, email, password});
+        newUser.password = await newUser.encryptPassword(password);
+        await newUser.save();
+        req.flash('success_msg', 'You are registered');
+        res.redirect('/users/signin');
+    }
 });
 
 module.exports = router;
